68485800475de287787eee4b3e3d6298bac65730
Hailey Francis <n01402670@unf.edu>
2023-05-22 17:05:00 -0400

    dropdown still not working

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,107 +1,140 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
-import { useState, useEffect } from "react"
-import {signIn, signOut, useSession, getProviders} from 'next-auth/react' //simplify login/logout flow
-
-function Nav() {
+const Nav = () => {
+  //const { data: session } = useSession();
   const isUserLoggedIn = true;
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
-  //sign in using google and next-auth
   useEffect(() => {
-    const setProviders = async () => {
-      const response = await getProviders();
-      setProviders(response);
-    }
-
-    setProviders();
-  }, [])
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
 
   return (
-    <nav className="flex-between w-full mb-16 pt-3">
-      <Link href="/" className="flex gap-2 flex-center">
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href='/' className='flex gap-2 flex-center'>
         <Image
-          src="assets/images/logo.svg"
-          alt="promptopia logo"
+          src='/assets/images/logo.svg'
+          alt='logo'
           width={30}
           height={30}
-          className="object-contain"
+          className='object-contain'
         />
-        <p className="logo_text">Promptopia</p>
+        <p className='logo_text'>Promptopia</p>
       </Link>
 
       {/* Desktop Navigation */}
-      <div className="sm:flex hidden">
+      <div className='sm:flex hidden'>
         {isUserLoggedIn ? (
-          <div className="flex gap3 md:gap-5">
-            <Link href="/create-prompt" className="black_btn">
-                Create Post
+          <div className='flex gap-3 md:gap-5'>
+            <Link href='/create-prompt' className='black_btn'>
+              Create Post
             </Link>
-            <button type="button" onClick={signOut} className="outline_btn">
+
+            <button type='button' onClick={signOut} className='outline_btn'>
               Sign Out
             </button>
-            <Link href="/profile">
-              <Image src="/assets/images/logo.svg"
-              width={37}
-              height={37}
-              className="rounded-full"
-              alt="profile"
+
+            <Link href='/profile'>
+              <Image
+                src='/assets/images/logo.svg'//{session?.user.image}
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='profile'
               />
             </Link>
           </div>
-        ):(
+        ) : (
           <>
-          {providers &&
-          //allows us to have all of the different providers and show buttons for the sign up
-          //only using one provider, googleAuth
-            Object.values(providers).map((provider) => ( 
-              <button type="button" key={provider.name} onClick={()=> signIn(provider.id)} className="black_btn">
-                Sign In
-              </button>
-            ))}
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className='black_btn'
+                >
+                  Sign in
+                </button>
+              ))}
           </>
         )}
       </div>
 
       {/* Mobile Navigation */}
-      <div className="sm:hidden flex relative">
+      <div className='sm:hidden flex relative'>
         {isUserLoggedIn ? (
-          <div className="flex">
-            <Image src="/assets/images/logo.svg"
-            width={37}
-            height={37}
-            className="rounded-full"
-            alt="profile"
-            onClick={() => setToggleDropdown((prev) => !prev)}
+          <div className='flex'>
+            <Image
+              src='/assets/images/logo.svg'//{session?.user.image}
+              width={37}
+              height={37}
+              className='rounded-full'
+              alt='profile'
+              onClick={() => setToggleDropdown((prev) => !prev)}
             />
-          {toggleDropdown && (
-            <div className="dropdown">
-              <Link href="/profile" className="dropdown_link" onClick={() => setToggleDropdown(false)}>
-                My Profile
-              </Link>
-            </div>
-          )}
+
+            {toggleDropdown && (
+              <div className='dropdown'>
+                <Link
+                  href='/profile'
+                  className='dropdown_link'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href='/create-prompt'
+                  className='dropdown_link'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type='button'
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className='mt-5 w-full black_btn'
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
-        ): (
+        ) : (
           <>
-          {providers &&
-          //allows us to have all of the different providers and show buttons for the sign up
-          //only using one provider, googleAuth
-            Object.values(providers).map((provider) => ( 
-              <button type="button" key={provider.name} onClick={()=> signIn(provider.id)} className="black_btn">
-                Sign In
-              </button>
-            ))}
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className='black_btn'
+                >
+                  Sign in
+                </button>
+              ))}
           </>
         )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
