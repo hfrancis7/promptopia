391c26582b763c137b998ae354994a87d07556d6
Hailey Francis <n01402670@unf.edu>
2023-05-23 18:37:28 -0400

    weird mongoose behavior error

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import Link from 'next/link';
 
-function Form() {
+function Form({type, post, setPost, submitting, handleSubmit}) {
   return (
-    <div>
-      
-    </div>
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+        </h1>
+    </section>
   )
 }
 
